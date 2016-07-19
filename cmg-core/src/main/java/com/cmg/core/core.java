a1ea626a8c9dbb30e4c35923a987927b8869babb
Sinh Vo Van <sinh.vo@pyramid-consulting.com>
2016-07-19 10:24:57 +0700

    add source

---
+++ cmg-core/src/main/java/com/cmg/core/core.java
@@ -0,0 +1,12 @@
+package com.cmg.core;
+
+/**
+ * This class is optional and represents the configuration for the cmg-core module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/cmg-core</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ */
+public class core {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
---
+++ cmg.cfyc/src/main/java/com/cmg/cfyc/cfyc.java
@@ -0,0 +1,12 @@
+package com.cmg.cfyc;
+
+/**
+ * This class is optional and represents the configuration for the cmg.cfyc module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/cmg.cfyc</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ */
+public class cfyc {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
